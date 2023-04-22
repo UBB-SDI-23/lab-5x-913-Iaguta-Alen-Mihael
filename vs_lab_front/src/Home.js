d187bb506900f4b266fb28861b0e06a3740546ae
Alen <alen.iaguta@stud.ubbcluj.ro>
2023-04-22 20:56:36 +0300

    Added tournament stuff

--- vs_lab_front/src/Home.js
+++ vs_lab_front/src/Home.js
@@ -7,7 +7,6 @@ export class Home extends Component{
             <div className="justify-content-center">
                 <h1>Hello muhfuckas</h1>
                 <p>This is my SDI chess project in my 4th semester.I wish you all Ramadan Kareem.</p>
-                
             </div>
         )
     }
