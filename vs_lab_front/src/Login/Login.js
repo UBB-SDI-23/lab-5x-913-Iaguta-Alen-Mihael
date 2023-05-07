956e7034252080c31b3bcb30c6b5c4aa8eb1907f
Alen <alen.iaguta@stud.ubbcluj.ro>
2023-05-07 15:18:31 +0300

    Created Login / Register pages now only My profile page and Log out page

---
+++ vs_lab_front/src/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Button, Form } from "react-bootstrap";
+
+export class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            user: {}
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        fetch(process.env.REACT_APP_API + 'userprofiles/login/' + this.state.email + '/' + this.state.password)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ user: data });
+            })
+            .then(() => {
+                alert("Successfully logged in!");
+                this.props.history.push(`/users/${this.state.user.userName}`);
+            })
+            .catch(error => {
+                alert('Failed to log in');
+                console.log(error);
+            });
+    }
+
+    render() {
+        const { email, password } = this.state;
+        const disabled = !email || !password;
+
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '65vh' }}>
+                <h1>Welcome back!</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="text" name="email" value={email} onChange={this.handleInputChange} required />
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="password" value={password} onChange={this.handleInputChange} required />
+                    </Form.Group>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '8vh' }}>
+                        <Button variant="primary" type="submit" disabled={disabled}>
+                            Log in
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        );
+    }
+}
